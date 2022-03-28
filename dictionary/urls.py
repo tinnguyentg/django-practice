81750117f3529d96a391ae01a88716ece5384792
tinnguyentg <tinnguyen121221@gmail.com>
2022-03-29 00:41:55 +0700

    Create Detail view

--- dictionary/urls.py
+++ dictionary/urls.py
@@ -1,8 +1,9 @@
 from django.urls import path
 
-from .views import Index
+from .views import Detail, Index
 
 app_name = "dictionary"
 urlpatterns = [
     path("", Index.as_view(), name="index"),
+    path("dictionaries/<slug:slug>", Detail.as_view(), name="detail"),
 ]
--- dictionary/views.py
+++ dictionary/views.py
@@ -5,6 +5,7 @@ from django.views import generic
 
 from .forms import DictionaryCreation
 from .models import Dictionary
+from .oxford import Entries, Lemmas
 
 
 class Index(generic.CreateView):
@@ -17,4 +18,25 @@ class Index(generic.CreateView):
     def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
         context = super().get_context_data(**kwargs)
         context["words"] = self.get_queryset()
+        return context
+
+
+class Detail(generic.DetailView):
+    model = Dictionary
+    template_name = "dictionary/detail.html"
+
+    def get_context_data(self, **kwargs: Any) -> Dict[str, Any]:
+        context = super().get_context_data(**kwargs)
+        word = context["object"].word.lower()
+        oxford_entries = Entries(word)
+        oxford_entries_result = oxford_entries.result()
+
+        if oxford_entries_result.get("error"):
+            context["object"].is_headword = False
+            context["object"].save()
+            oxford_lemmas = Lemmas(word)
+            context["oxford_lemmas"] = oxford_lemmas.result()
+        else:
+            context["oxford_entries"] = oxford_entries_result
+
         return context
